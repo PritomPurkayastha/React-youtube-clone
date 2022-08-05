1cdb1d5d2b1a54ca6d36056b016a52f8bc2a67cb
PritomPurkayastha <103960561+PritomPurkayastha@users.noreply.github.com>
2022-08-05 14:37:28 +0530

    wd

--- src/Components/Loading.js
+++ src/Components/Loading.js
@@ -1,16 +1,19 @@
-import React, { Suspense } from "react";
-import Skeleton from "@mui/material/Skeleton";
-import Stack from "@mui/material/Stack";
-import { Box } from "@mui/system";
+// import React, { Suspense } from "react";
+// import Skeleton from "@mui/material/Skeleton";
+// import Stack from "@mui/material/Stack";
+// import { Box } from "@mui/system";
+// import "./Loading.css"
+import Spinner from "react-bootstrap/Spinner";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./Loading.css"
 
 function Loading() {
   return (
-    <Box sx={{ width: 300 }} className="loading">
-      <Skeleton />
-      <Skeleton animation="wave" />
-      <Skeleton animation={false} />
-    </Box>
+    <div className="loading">
+      <Spinner animation="border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
+    </div>
   );
 }
 
